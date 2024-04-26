8790cc8edbfe67e909cf5ba7789d2a4e6bb74ac6
Gabriel dos Santos Silva <gabrieldossantossilva69@gmail.com>
2024-04-26 18:55:23 -0300

    primeiro commit (código com bugs)

---
+++ Models/Pessoa.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ExplorandoClasses.Models
+{
+    public class Pessoa
+    {
+        Pessoa(string nome, string sobrenome)
+        {
+            Nome = nome;
+            Sobrenome = sobrenome;
+        }
+        private string _nome;
+        private string _sobrenome;
+        private int _idade;
+
+        public string Nome 
+        { 
+            get => _nome.ToUpper();
+            
+            set
+            {
+                if (value == "")
+                {
+                    throw new ArgumentException("O nome não pode ser vazio.");
+                }
+                _nome = value;
+            } 
+        }
+        public string Sobrenome 
+        { 
+            get => _sobrenome.ToUpper(); 
+            
+            set
+            {
+                if (value == "")
+                {
+                    throw new ArgumentException("O sobrenome não pode estar vazio.");
+                }
+                _sobrenome = value;
+            } 
+        }
+        public string NomeCompleto 
+        {
+            get => $"{_nome} {_sobrenome}";
+        }
+        public int Idade 
+        {
+            get => _idade;
+            set
+            {
+                if(value == 0)
+                {
+                    throw new ArgumentException("A idade não pode ser igual a zero.");
+                }
+                _idade = value;
+            }
+        }
+        public void Apresentar()
+        {
+            Console.WriteLine($"Meu nome é {NomeCompleto} tenho {_idade} anos.");
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using ExplorandoClasses.Models;
+
+Pessoa pessoa1 = new Pessoa("Gabriel", "Santos");
+Pessoa pessoa2 = new Pessoa("Livia", "Santos");
+
+Curso cursoDeIngles = new Curso();
+cursoDeIngles.Nome = "Ingles";
+cursoDeIngles.Alunos = new List<Pessoa>();
+
+cursoDeIngles.AdicionarAluno(pessoa1);
+cursoDeIngles.AdicionarAluno(pessoa2);
+cursoDeIngles.ListarAlunos();
+
